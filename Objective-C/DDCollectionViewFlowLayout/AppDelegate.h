986f9ddb63a7b25bda846b690a002a2013d9b59c
刘隆昌 <liulongchang@langyue.org>
2016-08-12 09:12:50 +0800

    =

---
+++ Objective-C/DDCollectionViewFlowLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDCollectionViewFlowLayout
+//
+//  Created by DeJohn Dong on 15-2-12.
+//  Copyright (c) 2015年 DDKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C/DDCollectionViewFlowLayout/PhotoViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDCollectionViewFlowLayout
+//
+//  Created by DeJohn Dong on 15-2-12.
+//  Copyright (c) 2015年 DDKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewController : UICollectionViewController
+
+
+@end
+
